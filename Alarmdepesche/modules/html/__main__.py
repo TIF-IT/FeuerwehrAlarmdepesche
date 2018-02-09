82cbc1f6dbb54ddb527813d1f3f074534b8b6c83
Kai Trott <kaitrott@gmail.com>
2018-02-09 23:29:18 +0100

    Change active waiting in html module with 'waiting'

--- Alarmdepesche/modules/html/__main__.py
+++ Alarmdepesche/modules/html/__main__.py
@@ -4,8 +4,9 @@
 from Alarmdepesche.core import Core
 from Alarmdepesche.modules.html import html_module
 
+from waiting import wait
+
 c = Core()
 obj = c.get_instance(html_module.HtmlModule)
 assert obj
-while True:
-    pass
+wait(lambda : False)
