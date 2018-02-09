c620205934b96ee1f3a119b61090bd81d0663fb6
Linde Christian <linde.chris@googlemail.com>
2018-02-09 06:21:32 +0100

    modify html associative

--- Alarmdepesche/modules/html/html_module.py
+++ Alarmdepesche/modules/html/html_module.py
@@ -33,46 +33,49 @@ class HtmlModule(Api):
 
 
     def get_tasks(self):
-        db = MySQLdb.connect(config.mysql['host'], config.mysql['user'], config.mysql['passwd'], config.mysql['dbName'] )
+        db = MySQLdb.connect(config.mysql['host'], config.mysql['user'], config.mysql['passwd'], config.mysql['dbName'])
 #        db = self.get_db_connection()
-        cursor = db.cursor()
+        cursor = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
         try:
             sqlStatement = "select id, dbIN, messageID, Einsatzstichwort, AlarmiertesEinsatzmittel, Sondersignal, Einsatzbeginn, Einsatznummer, Target_Objekt, Target_Objekttyp, Target_StrasseHausnummer, Target_Segment, Target_PLZOrt, Target_Region, Target_Info, Name, Zusatz, TransTarget_Transportziel, TransTarget_Objekt, TransTarget_Objekttyp, TransTarget_StrasseHausnummer, TransTarget_PLZOrt from Alarmdepesche order by id desc limit 1";
             cursor.execute(sqlStatement)
             result = cursor.fetchone()
             db.close()
+
         except:
             print ("!Error in mysql statement")
             return jsonify({'Error':'Error in mysql statement'})
 
         if not result:
             print('No entries found in db')
-            return ''
+            return jsonify({'Error':'No entries found in db'}) 
 
-        alarmdepesche = { 'Default': { 'id'               : result[0]
-                                     , 'dbIN'             : str(result[1])
-                                     , 'messageID'        : result[2]
-                                     , 'Einsatzstichwort' : result[3]
-                                     , 'AlarmiertesEinsatzmittel' : self.strEncode(result[4])
-                                     , 'Sondersignal' : self.strEncode(result[5])
-                                     , 'Einsatzbeginn' : self.strEncode(result[6])
-                                     , 'Einsatznummer' : self.strEncode(result[7])
-                                     , 'Name' : self.strEncode(result[15])
-                                     , 'Zusatz' : self.strEncode(result[16])
+        print (result)
+
+        alarmdepesche = { 'Default': { 'id'               : result['id']
+                                     , 'dbIN'             : str(result['dbIN'])
+                                     , 'messageID'        : result['messageID']
+                                     , 'Einsatzstichwort' : result['Einsatzstichwort']
+                                     , 'AlarmiertesEinsatzmittel' : self.strEncode(result['AlarmiertesEinsatzmittel'])
+                                     , 'Sondersignal' : self.strEncode(result['Sondersignal'])
+                                     , 'Einsatzbeginn' : self.strEncode(result['Einsatzbeginn'])
+                                     , 'Einsatznummer' : self.strEncode(result['Einsatznummer'])
+                                     , 'Name' : self.strEncode(result['Name'])
+                                     , 'Zusatz' : self.strEncode(result['Zusatz'])
                                      }
-                        , 'Target' : { 'Objekt' : self.strEncode(result[8])
-                                     , 'Objekttyp' : self.strEncode(result[9])
-                                     , 'StrasseHausnummer' : self.strEncode(result[10])
-                                     , 'Segment' : self.strEncode(result[11])
-                                     , 'PLZOrt' : self.strEncode(result[12])
-                                     , 'Region' : self.strEncode(result[13])
-                                     , 'Info' : self.strEncode(result[14])
+                        , 'Target' : { 'Objekt' : self.strEncode(result['Target_Objekt'])
+                                     , 'Objekttyp' : self.strEncode(result['Target_Objekttyp'])
+                                     , 'StrasseHausnummer' : self.strEncode(result['Target_StrasseHausnummer'])
+                                     , 'Segment' : self.strEncode(result['Target_Segment'])
+                                     , 'PLZOrt' : self.strEncode(result['Target_PLZOrt'])
+                                     , 'Region' : self.strEncode(result['Target_Region'])
+                                     , 'Info' : self.strEncode(result['Target_Info'])
                                      }
-                        , 'TransportTarget' : { 'Transportziel' : self.strEncode(result[17])
-                                              , 'Objekt' : self.strEncode(result[18])
-                                              , 'Objekttyp' : self.strEncode(result[19])
-                                              , 'StrasseHausnummer' : self.strEncode(result[20])
-                                              , 'PLZOrt' : self.strEncode(result[21])
+                        , 'TransportTarget' : { 'Transportziel' : self.strEncode(result['TransTarget_Transportziel'])
+                                              , 'Objekt' : self.strEncode(result['TransTarget_Objekt'])
+                                              , 'Objekttyp' : self.strEncode(result['TransTarget_Objekttyp'])
+                                              , 'StrasseHausnummer' : self.strEncode(result['TransTarget_StrasseHausnummer'])
+                                              , 'PLZOrt' : self.strEncode(result['TransTarget_PLZOrt'])
                                               }
                         }
 
